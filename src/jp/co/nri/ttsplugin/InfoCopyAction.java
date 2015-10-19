4b8c43572f20f5cc13e15a19b7ea5b9aa3986007
turbou <turbou@i.softbank.jp>
2015-10-19 14:12:06 +0900

    import.

---
+++ src/jp/co/nri/ttsplugin/InfoCopyAction.java
@@ -0,0 +1,51 @@
+package jp.co.nri.ttsplugin;
+
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.widgets.Shell;
+
+import jp.co.tabocom.teratermstation.model.TargetNode;
+import jp.co.tabocom.teratermstation.ui.action.TeraTermStationAction;
+
+public class InfoCopyAction extends TeraTermStationAction {
+    protected InfoCopyAction(TargetNode node, Shell shell, ISelectionProvider selectionProvider) {
+        super("情報をコピー", "info.png", node, shell, selectionProvider);
+    }
+
+    @Override
+    public boolean isValid(TargetNode node) {
+        return true;
+    }
+
+    @Override
+    public void run() {
+        StringBuilder builder = new StringBuilder();
+        if (node.getChildren().isEmpty()) {
+            // 要は子供（サーバ号機）の場合
+            builder.append(String.format("%-8s", node.getHostName()));
+            builder.append(",");
+            builder.append(String.format("%-15s", node.getIpAddr()));
+            builder.append(",");
+            builder.append(node.getParent().getName());
+            builder.append(",");
+            builder.append(node.getName());
+        } else {
+            // 要は親（サーバ種別）の場合
+            builder.append(node.getName());
+            builder.append("\r\n");
+            for (TargetNode nd : node.getChildren()) {
+                builder.append(String.format("%-8s", nd.getHostName()));
+                builder.append(",");
+                builder.append(String.format("%-15s", nd.getIpAddr()));
+                builder.append(",");
+                builder.append(nd.getName());
+                builder.append("\r\n");
+            }
+        }
+        // クリップボード
+        Clipboard clipBoard = new Clipboard(shell.getDisplay());
+        clipBoard.setContents(new Object[] { builder.toString() }, new Transfer[] { TextTransfer.getInstance() });
+    }
+}
---
+++ src/jp/co/nri/ttsplugin/InfoCopyPlugin.java
@@ -0,0 +1,26 @@
+package jp.co.nri.ttsplugin;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.swt.widgets.Shell;
+
+import jp.co.tabocom.teratermstation.model.TargetNode;
+import jp.co.tabocom.teratermstation.plugin.TeraTermStationPlugin;
+import jp.co.tabocom.teratermstation.ui.action.TeraTermStationAction;
+
+public class InfoCopyPlugin implements TeraTermStationPlugin {
+
+    @Override
+    public List<TeraTermStationAction> getActions(TargetNode node, Shell shell, ISelectionProvider selectionProvider) {
+        return new ArrayList<TeraTermStationAction>(Arrays.asList(new InfoCopyAction(node, shell, selectionProvider)));
+    }
+
+    @Override
+    public PreferencePage getPreferencePage() {
+        return null;
+    }
+}
